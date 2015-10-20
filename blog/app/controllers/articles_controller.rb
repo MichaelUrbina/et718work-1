b0ce476ec363145e538a6d6293ec3754323b631d
MichaelUrbina <student@T14-01.et.qcc.edu>
2015-10-20 11:01:10 -0400

    5.4 to 5.11
    covers creation of one table. Next up will be Re-factoring

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,9 +1,38 @@
 class ArticlesController < ApplicationController
   def new
   end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def index
+    @articles = Article.all
+  end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
   end
-  
-end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+    def new
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.new(article_params)
+
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+    end
+
+    private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
